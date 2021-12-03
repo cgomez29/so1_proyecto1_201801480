3b48f82edb09d5e39a7720c625462710d7156355
cgomez29 <2912196670108@ingenieria.usac.edu.gt>
2021-12-03 14:36:49 -0600

    Test Gin golang

--- server-go/main.go
+++ server-go/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Prueba")
+	router := gin.Default()
+
+	//static failes
+	router.LoadHTMLFiles("public/index.html")
+
+	//routes
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"title": "api",
+		})
+	})
+
+	//middlewares
+
+	router.Run(":4000")
 }
